6e7d6a9f960d122d326a2232bfe1a9850fb87720
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2021-04-01 01:46:06 -0300

    Aula 02

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const router = require('./routes');
 
 server.set('view engine', 'ejs');
 
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static('public'));
 server.use(router);
 
