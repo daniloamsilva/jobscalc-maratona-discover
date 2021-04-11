c3b3486e4ead798bccd5822f149f99b06c103bab
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2021-04-11 20:14:03 -0300

    Search Input
    Foi adicionado um campo de busca na página inicial do site. Dessa forma é possível procurar um job pelo seu nome.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -14,3 +14,23 @@ for (let card of cards) {
     deleteForm.setAttribute('action', '/job/delete/' + cardId)
   }
 }
+
+const search_input = document.querySelector('#search_input');
+const jobs_list = document.querySelector('.cards');
+
+search_input.addEventListener('keyup', () => {
+  const filter = search_input.value.toUpperCase();
+  const cards = jobs_list.getElementsByClassName('card');
+
+  for (let i = 0; i < cards.length; i++) {
+    const name = cards[i].getElementsByClassName('name')[0].innerHTML;
+
+    if (name) {
+      if (name.toUpperCase().indexOf(filter) > -1) {
+        cards[i].style.display = "";
+      } else {
+        cards[i].style.display = "none";
+      }
+    }
+  }
+});
